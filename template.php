bee2b6f2712de9f5e9dfb0c678dc5472ab5b3bc8
Steven Wanderski <steven.wanderski@gmail.com>
2011-08-25 08:59:18 -0500

    added image style height and width attributes to template.php

--- template.php
+++ template.php
@@ -469,3 +469,32 @@ function bx_views_mini_pager($vars) {
     return theme('links', array('links' => $links, 'attributes' => array('class' => array('links', 'pager', 'views-mini-pager'))));
   }
 }
+
+/**
+ * Add width and height to image styled images
+ */
+function bx_image_style($variables) {
+  $style_name = $variables['style_name'];
+  $path = $variables['path'];
+
+  // theme_image() can only honor the $getsize parameter with local file paths.
+  // The derivative image is not created until it has been requested so the file
+  // may not yet exist, in this case we just fallback to the URL.
+  $style_path = image_style_path($style_name, $path);
+  if (!file_exists($style_path)) {
+    $style_path = image_style_url($style_name, $path);
+  }
+  $variables['path'] = $style_path;
+
+  if (is_file($style_path)) {
+    if (list($width, $height, $type, $attributes) = @getimagesize($style_path)) {
+      $variables['width'] = $width;
+      $variables['height'] = $height;
+    }
+  }
+  
+  return theme('image', $variables);
+}
+
+
+
