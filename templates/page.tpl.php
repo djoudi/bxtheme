1e70aac2ce2feebb8fac559bce5143b56e0f986c
Steven Wanderski <steven.wanderski@gmail.com>
2011-11-29 09:52:25 -0600

    stripped out page.tpl.php clutter

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,32 +1,6 @@
-<?php if ($page['help'] || ($show_messages && $messages)): ?>
-  <div id='console'><div class='limiter clearfix'>
-    <?php print render($page['help']); ?>
-    <?php if ($show_messages && $messages): print $messages; endif; ?>
-  </div></div>
-<?php endif; ?>
-
 <?php if ($page['header']): ?>
   <div id='header'><div class='limiter clearfix'>
     <?php print render($page['header']); ?>
-  </div></div>
-<?php endif; ?>
-
-<div id='branding'><div class='limiter clearfix'>
-  <?php if ($site_name): ?><h1 class='site-name'><?php print $site_name ?></h1><?php endif; ?>
-</div></div>
-
-<div id='navigation'><div class='limiter clearfix'>
-  <?php if (isset($main_menu)) : ?>
-    <?php print theme('links', array('links' => $main_menu, 'attributes' => array('class' => 'links main-menu'))) ?>
-  <?php endif; ?>
-  <?php if (isset($secondary_menu)) : ?>
-    <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('class' => 'links secondary-menu'))) ?>
-  <?php endif; ?>
-</div></div>
-
-<?php if ($page['highlighted']): ?>
-  <div id='highlighted'><div class='limiter clearfix'>
-    <?php print render($page['highlighted']); ?>
   </div></div>
 <?php endif; ?>
 
@@ -37,7 +11,6 @@
   <?php endif; ?>
 
   <div id='main-content' class='clearfix'>
-    <?php if ($breadcrumb) print $breadcrumb; ?>
     <?php if ($title): ?><h1 class='page-title'><?php print $title ?></h1><?php endif; ?>
     <?php if ($primary_local_tasks): ?><ul class='links clearfix'><?php print render($primary_local_tasks) ?></ul><?php endif; ?>
     <?php if ($secondary_local_tasks): ?><ul class='links clearfix'><?php print render($secondary_local_tasks) ?></ul><?php endif; ?>
